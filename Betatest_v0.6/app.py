b9c1d3d58c378d31279756382fbe122baa2d0572
Chaewon Song <81732426+chaewonS@users.noreply.github.com>
2022-07-04 07:25:49 +0900

    [채원] 7.4

--- Betatest_v0.6/app.py
+++ Betatest_v0.6/app.py
@@ -29,7 +29,7 @@ db = client['survey']
 def home():
    return render_template('login.html')
 
-#홈화면
+#홈화면_영어
 @app.route('/en')
 def home_en():
    return render_template('login_en.html')
@@ -71,7 +71,7 @@ def register():
             members.insert_one(post)
             return render_template("one_time.html", data=id)
 
-# 회원가입
+# 회원가입_영어
 @app.route('/register_en', methods=['GET','POST'])
 def register_en():
     if request.method == 'GET':
@@ -96,7 +96,6 @@ def register_en():
             flash("Already signed up for an account. Please check again!")
             return render_template("register_en.html", data=id)
         else :
-            #회원가입시 컬렉션 생성
             mongo.db.create_collection(id)  
         
         if not (id and pwd and pwd2):
@@ -107,35 +106,21 @@ def register_en():
             members.insert_one(post)
             return render_template("one_time_en.html", data=id)
 
-
+#ajax
 @app.route('/ajax', methods=['GET', 'POST'])
 def ajax():
     data = request.get_json()
-    #회원가입한 id와 같은 이름의 컬렉션 찾기
-    #list()함수 이용해서 첫 번째 키 가져오기(id 값)
     x_survey = list(data.values())[0]
     survey_result = mongo.db.get_collection(x_survey)
     del data['ID']
-    #해당 컬렉션에 data upload
     survey_result.insert_one(data)
     data.pop('_id')
     daily = data['1_formData1']
     daily_f = list(m['value'] for m in daily)
-    # if daily_f[3] == '1' and daily_f[4] == '1':
-    #     print("갤럭시와 갤럭시 워치 사용자입니다.")
-    #     members.update_one({'id': x_survey}, {'$set': {'daily': 1}})
-    # else:
-    #     if daily_f[3] == '2' and daily_f[4] == '2':
-    #         print("아이폰과 핏빗 사용자입니다.")
-    #         members.update_one({'id': x_survey}, {'$set': {'daily': 2}})
-    #     if (daily_f[3] == '1' and daily_f[4] == '2') or (daily_f[3] == '2' and daily_f[4] == '1'):
-    #         print("2번과 3번 조합이 틀립니다.")
     if daily_f[3] == '1':
-        # print('갤럭시 사용자입니다.')
         members.update_one({'id':x_survey}, {'$set':{'daily':1}})
     else:
         if daily_f[3] == '2':
-            # print('아이폰 사용자입니다.')
             members.update_one({'id':x_survey}, {'$set':{'daily':2}})
 
     return jsonify(result = "success", result2= data, result3=daily)
@@ -145,19 +130,16 @@ def ajax():
 def login():
     id = 'S' + request.form['id']
     pwd = request.form['pwd']
-    #form에서 가져온 id를 세션에 저장
     session['id'] = id
     user = members.find_one({'id':id}, {'pwd':pwd})
-    # print(wcount)
     if user is None:
         flash("접속하신 계정은 존재하지 않는 계정입니다.")
         return render_template('login.html')
     else:
         member_id = members.find_one({'id': id})
-        if member_id['pwd'] == pwd: #비밀번호가 맞는지 확인
+        if member_id['pwd'] == pwd: 
             daily_cal = member_id["daily"]
             cin_count = member_id["CIN_count"]
-            # print(daily_cal)
             if daily_cal == 1 : #갤럭시 사용자
                 return render_template('cal_test.html', sid=id, cin=cin_count)
             else:
@@ -168,7 +150,6 @@ def login():
                     asia_seoul = datetime.datetime.fromtimestamp(time.time(), pytz.timezone('Asia/Seoul'))
                     t = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
                     now = t[asia_seoul.today().weekday()]
-
                     if now == "토요일":
                         members.update_one({'id': id}, {'$inc': {'weekly_count': -1}}) #토요일 로그인 -> count가 1씩 감소
                         return render_template('weekly.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
@@ -177,42 +158,54 @@ def login():
                             return render_template('weekly.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
                         else:
                             if member_id['daily'] == 1:
-                                return render_template('weekly.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
-
-                                # return render_template('gal_daily.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
+                                return render_template('gal_daily.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
                             else:
                                 if member_id['daily'] == 2:
-                                    return render_template('weekly.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
-
-                                    # return render_template('daily.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)                    
+                                    return render_template('daily.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)                    
         else: #비밀번호가 틀리면
             flash('비밀번호가 일치하지 않습니다.')
             return render_template('login.html')
 
-#갤럭시 사용자 - 캘린더 보여주기 
-# @app.route('/cal', methods = ['POST'])
-# def cal():
-#     member_id = members.find_one({'id': id})
-#     count = member_id['submit_count']
-#     fcount = member_id['attach_count']
-#     wcount = member_id['weekly_count']
-#     asia_seoul = datetime.datetime.fromtimestamp(time.time(), pytz.timezone('Asia/Seoul'))
-#     t = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
-#     now = t[asia_seoul.today().weekday()]
-
-#     if now == "금요일":
-#         members.update_one({'id': id}, {'$inc': {'weekly_count': -1}}) #토요일 로그인 -> count가 1씩 감소
-#         return render_template('weekly.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
-#     else:
-#         if member_id['weekly_count'] > 0:
-#             return render_template('weekly.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
-#         else:
-#             if member_id['daily'] == 1:
-#                 return render_template('gal_daily.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
-#             else:
-#                 if member_id['daily'] == 2:
-#                     return render_template('daily.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)    
-
+#로그인_영어
+@app.route('/user/login_en', methods = ['POST'])
+def login_en():
+    id = 'S' + request.form['id']
+    pwd = request.form['pwd']
+    session['id'] = id
+    user = members.find_one({'id':id}, {'pwd':pwd})
+    if user is None:
+        flash("The account you accessed does not exist")
+        return render_template('login_en.html')
+    else:
+        member_id = members.find_one({'id': id})
+        if member_id['pwd'] == pwd:
+            daily_cal = member_id["daily"]
+            cin_count = member_id["CIN_count"]
+            if daily_cal == 1 : #갤럭시 사용자
+                return render_template('cal_test_en.html', sid=id, cin=cin_count)
+            else:
+                if daily_cal == 2: #아이폰 사용자
+                    count = member_id['submit_count']
+                    fcount = member_id['attach_count']
+                    wcount = member_id['weekly_count']
+                    asia_seoul = datetime.datetime.fromtimestamp(time.time(), pytz.timezone('Asia/Seoul'))
+                    t = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+                    now = t[asia_seoul.today().weekday()]
+                    if now == "토요일":
+                        members.update_one({'id': id}, {'$inc': {'weekly_count': -1}}) #토요일 로그인 -> count가 1씩 감소
+                        return render_template('weekly_en.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
+                    else:
+                        if member_id['weekly_count'] > 0:
+                            return render_template('weekly_en.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
+                        else:
+                            if member_id['daily'] == 1:
+                                return render_template('gal_daily_en.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)
+                            else:
+                                if member_id['daily'] == 2:
+                                    return render_template('daily_en.html', sid=id, cnt=count, fcnt=fcount, wcnt=wcount)                    
+        else: #비밀번호가 틀리면
+            flash('The password you entered does not match.')
+            return render_template('login_en.html')
 
 #로그아웃
 @app.route('/logout')
@@ -221,7 +214,7 @@ def logout():
     session.pop('id', None)
     return render_template('login.html')
 
-#daily survey-iphone user
+#Daily_아이폰 사용자
 @app.route('/moody', methods=['POST'])
 def moody():  
     data = request.files['data']
@@ -230,11 +223,9 @@ def moody():
     evl = eval(l)
     s_id = evl['sid']
     mood = evl['mood']
-    #print("mood is ", mood)
     md = { "mood": mood }
     survey_coll = mongo.db.get_collection(s_id)
     survey_coll.insert_one(md)
-    
     if 'filed' in request.files:
         f = request.files['filed']
         print("file1 is", f)
@@ -252,11 +243,43 @@ def moody():
         members.update_one({'id': s_id}, {'$inc': {'attach_count': 1}})
         fs2.put(contents, filename=fname2)
 
-    
     members.update_one({'id': s_id}, {'$inc': {'submit_count': 1}})
     return render_template('weekly.html')
 
-#daily survey-galaxy user
+#Daily_아이폰 사용자_영어
+@app.route('/moody_en', methods=['POST'])
+def moody_en():  
+    data = request.files['data']
+    st = data.read()
+    l = st.decode()
+    evl = eval(l)
+    s_id = evl['sid']
+    mood = evl['mood']
+    md = { "mood": mood }
+    survey_coll = mongo.db.get_collection(s_id)
+    survey_coll.insert_one(md)
+
+    if 'filed' in request.files:
+        f = request.files['filed']
+        print("file1 is", f)
+        contents = f.read()
+        fs = gridfs.GridFS(db, s_id)
+        fname = f.filename
+        members.update_one({'id': s_id}, {'$inc': {'attach_count': 1}})
+        fs.put(contents, filename=fname)
+    if 'filed2' in request.files:
+        f2 = request.files['filed2']
+        print("file2 is", f2)
+        contents = f2.read()
+        fs2 = gridfs.GridFS(db, s_id)
+        fname2 = f2.filename
+        members.update_one({'id': s_id}, {'$inc': {'attach_count': 1}})
+        fs2.put(contents, filename=fname2)
+
+    members.update_one({'id': s_id}, {'$inc': {'submit_count': 1}})
+    return render_template('weekly_en.html')
+
+#Daily_갤럭시 사용자
 @app.route('/moody2', methods=['POST'])
 def moody2():  
     data = request.files['data']
@@ -283,19 +306,45 @@ def moody2():
     members.update_one({'id': s_id}, {'$inc': {'submit_count': 1}})
     return render_template('weekly.html')
 
+#Daily_갤럭시 사용자_영어
+@app.route('/moody2_en', methods=['POST'])
+def moody2_en():  
+    data = request.files['data']
+    st = data.read()
+    l = st.decode()
+    evl = eval(l)
+    s_id = evl['sid']
+    mood = evl['mood']
+    md = { "mood": mood }
+    survey_coll = mongo.db.get_collection(s_id)
+    survey_coll.insert_one(md)
+    
+    if 'filed[]' in request.files:
+        f = request.files.getlist('filed[]')
+        for file in f:
+          print("file is", file)
+          contents = file.read()
+          fs = gridfs.GridFS(db, s_id)
+          fname = file.filename
+          fs.put(contents, filename=fname)
+        members.update_one({'id': s_id}, {'$inc': {'attach_count': 1}})
+    
+    members.update_one({'id': s_id}, {'$inc': {'submit_count': 1}})
+    return render_template('weekly_en.html')
+
 #마지막 페이지
 @app.route('/final', methods=['GET', 'POST'])
 def final():
     session.pop('id', None)
     return render_template("final.html")
 
-#마지막 페이지
+#마지막 페이지_영어
 @app.route('/final_en', methods=['GET', 'POST'])
 def final_en():
     session.pop('id', None)
     return render_template("final_en.html")
 
-#weekly survey
+#Weekly
 @app.route('/weekly', methods=['GET', 'POST'])
 def weekly():
     data = request.get_json()
@@ -313,12 +362,29 @@ def weekly():
         if member_id['daily'] == 2:
             return jsonify(render_template("daily.html", sid=x_survey, cnt=count, fcnt=fcount))
 
+#Weekly_영어
+@app.route('/weekly_en', methods=['GET', 'POST'])
+def weekly_en():
+    data = request.get_json()
+    x_survey = list(data.values())[0]
+    survey_result = mongo.db.get_collection(x_survey)
+    del data['ID']
+    survey_result.insert_one(data)
+    data.pop('_id')
+    member_id = members.find_one({'id': x_survey})
+    count = member_id['submit_count']
+    fcount = member_id['attach_count']
+    if member_id['daily'] == 1:
+        return jsonify(render_template("gal_daily_en.html", sid=x_survey, cnt=count, fcnt=fcount))
+    else:
+        if member_id['daily'] == 2:
+            return jsonify(render_template("daily_en.html", sid=x_survey, cnt=count, fcnt=fcount))
+
+#캘린더_갤럭시 사용자
 @app.route('/ajax3', methods=['GET', 'POST'])
 def ajax3():
     data = request.get_json()
     x_survey = list(data.values())[0]
-    #회원가입한 id와 같은 이름의 컬렉션 찾기
-    #list()함수 이용해서 첫 번째 키 가져오기(id 값)
     member_id = members.find_one({'id': x_survey})
     count = member_id['submit_count']
     fcount = member_id['attach_count']
@@ -339,31 +405,47 @@ def ajax3():
                 if member_id['daily'] == 2:
                     return jsonify(render_template('weekly.html', sid=x_survey, cnt=count, fcnt=fcount, wcnt=wcount))
 
+#캘린더_갤럭시 사용자_영어
+@app.route('/ajax3_en', methods=['GET', 'POST'])
+def ajax3_en():
+    data = request.get_json()
+    x_survey = list(data.values())[0]
+    member_id = members.find_one({'id': x_survey})
+    count = member_id['submit_count']
+    fcount = member_id['attach_count']
+    wcount = member_id["weekly_count"]
+    asia_seoul = datetime.datetime.fromtimestamp(time.time(), pytz.timezone('Asia/Seoul'))
+    t = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+    now = t[asia_seoul.today().weekday()]
+    if now == "토요일":
+        members.update_one({'id': x_survey}, {'$inc': {'weekly_count': -1}}) #토요일 로그인 -> count가 1씩 감소
+        return jsonify(render_template('weekly_en.html', sid=x_survey, cnt=count, fcnt=fcount, wcnt=wcount))
+    else:
+        if member_id['weekly_count'] > 0:
+            return jsonify(render_template('weekly_en.html', sid=x_survey, cnt=count, fcnt=fcount, wcnt=wcount))
+        else:
+            if member_id['daily'] == 1:
+                return jsonify(render_template('gal_daily_en.html', sid=x_survey, cnt=count, fcnt=fcount, wcnt=wcount))
+            else:
+                if member_id['daily'] == 2:
+                    return jsonify(render_template('weekly_en.html', sid=x_survey, cnt=count, fcnt=fcount, wcnt=wcount))
 
-
-    # data = request.get_data()
-    # print(data) 
-    # d = request.get_json()
-    # print(d)
-    # return jsonify(render_template("daily.html"))
-    
 #팝업창
 @app.route('/pop.html', methods=['GET'])
 def window_pop():
     return render_template("pop.html")
 
-#팝업창
+#팝업창_영어
 @app.route('/pop_en.html', methods=['GET'])
 def window_pop_en():
     return render_template("pop_en.html")
 
-#백그라운드 실행-토요일마다
+#백그라운드 실행
 asia_seoul = datetime.datetime.fromtimestamp(time.time(), pytz.timezone('Asia/Seoul'))
 today = asia_seoul.strftime("%Y%m%d")
 
 def count():
     asia_seoul = datetime.datetime.fromtimestamp(time.time(), pytz.timezone('Asia/Seoul'))
-    # print(asia_seoul.today())
     t = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
     now = t[asia_seoul.today().weekday()]
     if now == '토요일':
@@ -373,8 +455,6 @@ def count():
 IoT_1 = "http://114.71.220.59:7579"
 
 def countCIN(serverName, aeName, today):
-    # asia_seoul = datetime.datetime.fromtimestamp(time.time(), pytz.timezone('Asia/Seoul'))
-    # today = asia_seoul.strftime("%Y%m%d")
     cra = '&cra=' + str(int(today) - 1) + 'T150000'
     crb = '&crb=' + today + 'T145959'
     
@@ -396,8 +476,6 @@ def countCIN(serverName, aeName, today):
         return total
 
     except:
-        # print("mobile: ", response1)
-        # print("watch: ", response2)
         return None
 
 def getCountDict(today):
@@ -413,11 +491,7 @@ def getCountDict(today):
             
     for k,v in dict_CIN.items():
         k=str(k)
-        # print(v)
-        # print("{}:{}".format(k,v))
         member_id = members.find_one({'id': k})
-        # print(member_id)
-        # members.update_one({'id':k}, {'$set':{'CIN_count':v}})
         members.update_one({'id':k}, {'$set':{'CIN_count':v}})
     return dict_CIN
     
@@ -426,7 +500,7 @@ if __name__ == '__main__':
     sched = BackgroundScheduler(daemon=True)
     asia_seoul = datetime.datetime.fromtimestamp(time.time(), pytz.timezone('Asia/Seoul'))
     today = asia_seoul.strftime("%Y%m%d")
-    print(type(today))
+
     sched.add_job(count, 'cron', hour="00", minute="00", id="test_1") #토요일마다
     sched.add_job(getCountDict, 'cron', hour="23", minute="59", id="test_2", args=[today])
     sched.start()
