0893524aa44c0b60f9f59bc23d2aaa1d663183ed
Changwon Lee <89180067+LeeChangwon-AT-SCH@users.noreply.github.com>
2022-07-10 16:41:06 +0900

    Update app.py

--- Betatest_v1.0/app.py
+++ Betatest_v1.0/app.py
@@ -566,4 +566,4 @@ if __name__ == '__main__':
 
     # from waitress import serve
     # serve(app, host="0.0.0.0", port=2017)
-    app.run(host='0.0.0.0', port=2019)
+    app.run(host='0.0.0.0', port=2017)
